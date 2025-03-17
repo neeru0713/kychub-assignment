285eed4b29f2a4dba1cb88ff803435c0e3c1c099
neeru0713 <neerurani1307@gmail.com>
2025-03-17 19:28:28 +0530

    react router setup , made components folder/file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'
+import { ProductDetails } from "./components/product/ProductDetails";
+import { CompareProducts } from "./components/product/CompareProducts";
+import { Navbar } from "./components/nav/Navbar";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-   
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductDetails />}></Route>
+        <Route path="/compare=products" element={<CompareProducts />}></Route>
+      </Routes>
+  </Router>
   )
 }
 
